757248175b02f334b3dba94982346759d889ae68
Amsawardan <amsawardan01@gmail.com>
2025-09-24 10:39:46 +0530

    Created Folder system for frontend and backend

---
+++ backend/src/main/java/com/rajpharmacy/sell/PharmacyManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rajpharmacy.sell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyManagmentSystemApplication.class, args);
+	}
+
+}
