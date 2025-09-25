960071cf3cd9df98586bb39b34c960a914faa999
NadhirHajireen <nadhirhajireen2019@gmail.com>
2025-09-25 17:28:55 +0530

    Changed File Format

--- src/main/java/com/Pharmacy_Management_System/Raj_Pharmacy/RajPharmacyApplication.java
+++ src/main/java/com/Pharmacy_Management_System/Raj_Pharmacy/RajPharmacyApplication.java
@@ -1,13 +1,13 @@
-package com.rajpharmacy.sell;
+package com.Pharmacy_Management_System.Raj_Pharmacy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PharmacyManagmentSystemApplication {
+public class RajPharmacyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PharmacyManagmentSystemApplication.class, args);
+		SpringApplication.run(RajPharmacyApplication.class, args);
 	}
 
 }
--- src/test/java/com/Pharmacy_Management_System/Raj_Pharmacy/RajPharmacyApplicationTests.java
+++ src/test/java/com/Pharmacy_Management_System/Raj_Pharmacy/RajPharmacyApplicationTests.java
@@ -1,10 +1,10 @@
-package com.rajpharmacy.sell;
+package com.Pharmacy_Management_System.Raj_Pharmacy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PharmacyManagmentSystemApplicationTests {
+class RajPharmacyApplicationTests {
 
 	@Test
 	void contextLoads() {
