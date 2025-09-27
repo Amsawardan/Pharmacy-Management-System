0b1d700c8fa812e837ba6698d97a0723019d611d
NadhirHajireen <nadhirhajireen2019@gmail.com>
2025-09-27 16:33:30 +0530

    Added Create functionality for Order

--- src/main/java/com/Pharmacy_Management_System/Raj_Pharmacy/controller/OrderController.java
+++ src/main/java/com/Pharmacy_Management_System/Raj_Pharmacy/controller/OrderController.java
@@ -3,8 +3,8 @@ package com.Pharmacy_Management_System.Raj_Pharmacy.controller;
 import com.Pharmacy_Management_System.Raj_Pharmacy.model.Order;
 import com.Pharmacy_Management_System.Raj_Pharmacy.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -13,8 +13,18 @@ public class OrderController {
     @Autowired
     OrderService orderservice;
 
-    @RequestMapping("/orders")
+    @GetMapping("/orders")
     public List<Order> getOrders() {
         return orderservice.getOrders();
+    }
+
+    @GetMapping("/orders/{orderId}")
+    public Order getOrderById(@PathVariable int orderId) {
+        return orderservice.getOrderById(orderId);
+    }
+
+    @PostMapping("/orders")
+    public void addOrder(@RequestBody Order order){
+        orderservice.addOrder( order);
     }
 }
--- src/main/java/com/Pharmacy_Management_System/Raj_Pharmacy/service/OrderService.java
+++ src/main/java/com/Pharmacy_Management_System/Raj_Pharmacy/service/OrderService.java
@@ -2,18 +2,33 @@ package com.Pharmacy_Management_System.Raj_Pharmacy.service;
 
 import com.Pharmacy_Management_System.Raj_Pharmacy.model.Order;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class OrderService {
 
-    List<Order> orders = Arrays.asList
-            (new Order(101,"Penadol",100),
+    List<Order> orders = new ArrayList<>(Arrays.asList(
+            new Order(101,"Penadol",100),
             new Order(102,"Asprine",1600),
-            new Order(103,"Digene",1200));
+            new Order(103,"Digene",1200)));
 
     public List<Order> getOrders(){
         return orders;
+    }
+
+    public Order getOrderById(int OrderId) {
+        for(int i = 0; i < orders.size(); i++){
+            if(orders.get(i).getOrderId() == OrderId){
+                return orders.get(i);
+            }
+        }
+        return null;
+    }
+
+    public void addOrder(Order order){
+        orders.add(order);
     }
 }
